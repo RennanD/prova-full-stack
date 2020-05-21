de37edd6dc830a58671ff25623eb2a4fbdd3f564
rennand <rplaykl@gmail.com>
2020-05-21 12:51:38 -0300

    feat: backend - Structure database

---
+++ backend/src/database/migrations/1590075094303-CreateToDos.ts
@@ -0,0 +1,61 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateToDos1590075094303 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    	 await queryRunner.createTable(
+      new Table({
+        name: 'todos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+          	name: "start_date",
+          	type: "timestamp",
+          	isNullable: false
+          },
+          {
+          	name: "end_date",
+          	type: "timestamp",
+          	isNullable: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    	await queryRunner.dropTable('todos');
+    }
+
+}
